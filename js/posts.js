bd1ca78a9d4490269a1f9c26325707e86031ec4b
Ethan <69441307+ogradye@users.noreply.github.com>
2021-11-10 04:43:09 -0500

    Create posts.js

---
+++ js/posts.js
@@ -0,0 +1,48 @@
+const getUserPosts = async (userID) => {
+	const res = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userID}`);
+	const userPosts = await res.json();
+	return userPosts;
+}
+
+const getPoster = async (userID) => {
+	const res = await fetch(`https://jsonplaceholder.typicode.com/users/${userID}`);
+	const user = await res.json();
+	return user;
+}
+
+const url = window.location.search;
+
+const params = new URLSearchParams(url);
+
+const generatePost = (post) => {
+	const postContainer = document.getElementById('postsContainer');
+	const postEl = document.createElement('div');
+	const postHtml = `<h1>${post.title}</h1><p>${post.body}</p>`;
+
+	postEl.classList.add('post');
+
+	postEl.innerHTML = postHtml;
+
+	postContainer.appendChild(postEl);
+}
+
+const generateUser = (user) => {
+	const userContainer = document.getElementById('user');
+	const userHtml = `${user.name} Posts`;
+
+	userContainer.innerHTML = userHtml;
+}
+
+if(params.has('user')){
+	getPoster(params.get('user'))
+	.then(user => {
+		generateUser(user);
+	});
+
+	getUserPosts(params.get('user'))
+	.then(posts => {
+		posts.forEach(post => {
+			generatePost(post);
+		})
+	});
+}
